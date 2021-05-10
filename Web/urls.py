99d4f9f95d0456812284c0b7f6a42b32dd986e63
TNBL265 <long_tran@mymail.sutd.edu.sg>
2021-05-10 09:59:40 +0800

    Fix static files not loading

--- Web/urls.py
+++ Web/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 from django.conf.urls import url
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
